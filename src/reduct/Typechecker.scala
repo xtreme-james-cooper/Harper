d0d17550a7fd44c5cca0dc12db3ef61fa5a97e50
James Cooper <jamescooper@gmail.com>
2012-11-20 18:26:13 -0500

    James C - eliminated typeExpand

--- src/reduct/Typechecker.scala
+++ src/reduct/Typechecker.scala
@@ -44,6 +44,9 @@ import model.Unknown
 object Typechecker {
 
   type Constraint = (Type, Type)
+  type Env = Map[String, Type]
+
+  def baseEnv : (Env, Env) = (Map(), Map())
 
   //not semantically a var; done this way to save the effort of piping it around
   var typeVarCounter : Int = 0
@@ -53,126 +56,121 @@ object Typechecker {
     Unknown(typeVarCounter)
   }
 
-  def newBindingName : String = {
-    typeVarCounter = typeVarCounter + 1
-    "#" + typeVarCounter //safe because #2 is unparseable as a var
-  }
-
-  def assembleConstraints(e : Expr)(env : Env) : (Type, List[Constraint]) = {
+  def assembleConstraints(e : Expr)(env : Env)(tyenv : Env) : (Type, List[Constraint]) = {
     //    println(e + " in " + env)
-    val (t, cs) = assembleConstraints_(e)(env)
+    val (t, cs) = assembleConstraints_(e)(env)(tyenv)
     //    println(t + " and " + cs)
     (t, cs)
   }
 
-  def assembleConstraints_(e : Expr)(env : Env) : (Type, List[Constraint]) = e match {
+  def assembleConstraints_(e : Expr)(env : Env)(tyenv : Env) : (Type, List[Constraint]) = e match {
     case Var(x) => (env(x), Nil)
     case Z      => (Nat, Nil)
     case Triv   => (UnitTy, Nil)
     case S(n) => {
-      val (t, cs) = assembleConstraints(n)(env)
+      val (t, cs) = assembleConstraints(n)(env)(tyenv)
       (t, (t, Nat) :: cs)
     }
     case Lam(v, t, e) => {
-      val (t2, cs) = assembleConstraints(e)(env + (v -> t))
-      (Arrow(t, t2), cs)
+      val t1 = t.swap(tyenv)
+      val (t2, cs) = assembleConstraints(e)(env + (v -> t1))(tyenv)
+      (Arrow(t1, t2), cs)
     }
     case App(e1, e2) => {
-      val (Arrow(t1, t2), cs1) = assembleConstraints(e1)(env)
-      val (t3, cs2) = assembleConstraints(e2)(env)
+      val (Arrow(t1, t2), cs1) = assembleConstraints(e1)(env)(tyenv)
+      val (t3, cs2) = assembleConstraints(e2)(env)(tyenv)
       (t2, (t1, t3) :: cs1 ++ cs2)
     }
     case Fix(v, e) => {
       val t = env(v) //Guarenteed to be there by construction, since Fixes are only built by defs, which enhance the environment 
-      val (t2, cs) = assembleConstraints(e)(env + (v -> t))
+      val (t2, cs) = assembleConstraints(e)(env + (v -> t))(tyenv)
       (t, (t, t2) :: cs)
     }
     case PairEx(e1, e2) => {
-      val (t1, cs1) = assembleConstraints(e1)(env)
-      val (t2, cs2) = assembleConstraints(e2)(env)
+      val (t1, cs1) = assembleConstraints(e1)(env)(tyenv)
+      val (t2, cs2) = assembleConstraints(e2)(env)(tyenv)
       (Product(t1, t2), cs1 ++ cs2)
     }
     case InL(i) => {
       val hole = newUnknown
-      val (t3, cs) = assembleConstraints(i)(env)
+      val (t3, cs) = assembleConstraints(i)(env)(tyenv)
       (Sum(t3, hole), cs)
     }
     case InR(i) => {
       val hole = newUnknown
-      val (t3, cs) = assembleConstraints(i)(env)
+      val (t3, cs) = assembleConstraints(i)(env)(tyenv)
       (Sum(hole, t3), cs)
     }
     case Match(e, rs) => {
-      val (t1, cs1) = assembleConstraints(e)(env)
-      val (t2, cs2) = typeverify(rs)(t1)(env)
+      val (t1, cs1) = assembleConstraints(e)(env)(tyenv)
+      val (t2, cs2) = typeverify(rs)(t1)(env)(tyenv)
       (t2, cs1 ++ cs2)
     }
     case Fold(mu, t, e) => {
-      val (t2, cs) = assembleConstraints(e)(env)
-      (Inductive(mu, t), (t2, t.swap(mu, Inductive(mu, t))) :: cs)
+      val t1 = t.swap(tyenv)
+      val (t2, cs) = assembleConstraints(e)(env)(tyenv)
+      (Inductive(mu, t1), (t2, t1.swap(mu, Inductive(mu, t1))) :: cs)
     }
     case Unfold(e) => {
-      val hole = newUnknown
-      val bind = newBindingName
-      val (Inductive(mu, t), cs) = assembleConstraints(e)(env)
+      val (Inductive(mu, t), cs) = assembleConstraints(e)(env)(tyenv)
       (t.swap(mu, Inductive(mu, t)), cs)
     }
     case TypeLam(t, e) => {
-      val (t1, cs) = assembleConstraints(e)(env)
+      val (t1, cs) = assembleConstraints(e)(env)(tyenv)
       (ForAll(t, t1), cs)
     }
     case TypeApp(e, t) => {
-      val (ForAll(x, t1), cs) = assembleConstraints(e)(env)
-      (t1.swap(x, t), cs)
+      val (ForAll(x, t1), cs) = assembleConstraints(e)(env)(tyenv)
+      (t1.swap(x, t.swap(tyenv)), cs)
     }
   }
-
+  
   //t is the type that the pattern is expected to have; under that assumption, it produces some type
-  def typeverify(rs : List[Rule])(t : Type)(env : Env) : (Type, List[Constraint]) =
-    rs.map(r => typeverify(r, t, env)).reduce[(Type, List[Constraint])]({
+  def typeverify(rs : List[Rule])(t : Type)(env : Env)(tyenv : Env) : (Type, List[Constraint]) =
+    rs.map(r => typeverify(r, t, env, tyenv)).reduce[(Type, List[Constraint])]({
       case ((t1, cs1), (t2, cs2)) => (t1, (t1, t2) :: cs1 ++ cs2)
     })
 
-  def typeverify(r : Rule, t : Type, env : Env) : (Type, List[Constraint]) = {
-    val (bind, cs1) = typeverify(r.p, t, env);
-    val (t0, cs2) = assembleConstraints(r.b)(env ++ bind)
+  def typeverify(r : Rule, t : Type, env : Env, tyenv : Env) : (Type, List[Constraint]) = {
+    val (bind, cs1) = typeverify(r.p, t, env, tyenv);
+    val (t0, cs2) = assembleConstraints(r.b)(env ++ bind)(tyenv)
     (t0, cs1 ++ cs2)
   }
 
   //Trying to match against t, it produces a list of variable-type bindings
-  def typeverify(p : Pattern, t : Type, env : Env) : (Map[String, Type], List[Constraint]) = p match {
+  def typeverify(p : Pattern, t : Type, env : Env, tyenv : Env) : (Map[String, Type], List[Constraint]) = p match {
     case WildPat   => (Map(), Nil)
     case VarPat(x) => (Map(x -> t), Nil)
     case TrivPat   => (Map(), (t, UnitTy) :: Nil)
     case ZPat      => (Map(), (t, Nat) :: Nil)
     case SPat(p) => {
-      val (bind, cs) = typeverify(p, Nat, env)
+      val (bind, cs) = typeverify(p, Nat, env, tyenv)
       (bind, (t, Nat) :: cs)
     }
     case PairPat(p1, p2) => {
       val t1 = newUnknown
       val t2 = newUnknown
-      val (p1binds, cs1) = typeverify(p1, t1, env)
-      val (p2binds, cs2) = typeverify(p2, t2, env)
+      val (p1binds, cs1) = typeverify(p1, t1, env, tyenv)
+      val (p2binds, cs2) = typeverify(p2, t2, env, tyenv)
       if ((p1binds.keySet & p2binds.keySet).isEmpty) (p1binds ++ p2binds, (t, Product(t1, t2)) :: cs1 ++ cs2)
       else throw new Exception("Overlapping pattern variables")
     }
     case InLPat(p) => {
       val t1 = newUnknown
       val t2 = newUnknown
-      val (bind, cs) = typeverify(p, t1, env)
+      val (bind, cs) = typeverify(p, t1, env, tyenv)
       (bind, (t, Sum(t1, t2)) :: cs)
     }
     case InRPat(p) => {
       val t1 = newUnknown
       val t2 = newUnknown
-      val (bind, cs) = typeverify(p, t2, env)
+      val (bind, cs) = typeverify(p, t2, env, tyenv)
       (bind, (t, Sum(t1, t2)) :: cs)
     }
   }
 
-  def typecheck(e : Expr)(env : Env) : Type = {
-    val (t, cs) = assembleConstraints(e)(env)
+  def typecheck(e : Expr)(env : Env)(tyenv : Env) : Type = {
+    val (t, cs) = assembleConstraints(e)(env)(tyenv)
     verifyConstraints(t, cs)
   }
 
@@ -189,37 +187,13 @@ object Typechecker {
     cs.map({ case (a, b) => (a.swap(unkId, t2), b.swap(unkId, t2)) })
 
   def typecheck(d : Defn)(env : Env, tyenv : Env) : (Env, Env) = d match {
-    case ExprDefn(n, b, args) => (env + (n -> typecheck(typeExpand(b, tyenv))(env ++ args.map({ case (v, t) => (v, t.swap(tyenv)) }))), tyenv)
+    case ExprDefn(n, b, args) => (env + (n -> typecheck(b)(env ++ args.map({ case (v, t) => (v, t.swap(tyenv)) }))(tyenv)), tyenv)
     case TypeDefn(n, t)       => (env, tyenv + (n -> t.swap(tyenv)))
   }
 
-  //expand type synonyms in tyenv
-  def typeExpand(e : Expr, tyenv : Map[String, Type]) : Expr = e match {
-    //Safe to ignore bindings in Fold and TyLam because TySyns must be uppercase strings and TyVars must be lowercase
-    case Var(x)         => Var(x)
-    case Z              => Z
-    case S(n)           => S(typeExpand(n, tyenv))
-    case Lam(v, t, e)   => Lam(v, t.swap(tyenv), typeExpand(e, tyenv))
-    case App(e1, e2)    => App(typeExpand(e1, tyenv), typeExpand(e2, tyenv))
-    case Fix(v, e)      => Fix(v, typeExpand(e, tyenv))
-    case Triv           => Triv
-    case PairEx(e1, e2) => PairEx(typeExpand(e1, tyenv), typeExpand(e2, tyenv))
-    case InL(e)         => InL(typeExpand(e, tyenv))
-    case InR(e)         => InR(typeExpand(e, tyenv))
-    case Match(e, rs)   => Match(typeExpand(e, tyenv), rs.map({ case Rule(p, b) => Rule(p, typeExpand(b, tyenv)) }))
-    case Fold(mu, t, e) => Fold(mu, t.swap(tyenv), typeExpand(e, tyenv))
-    case Unfold(e)      => Unfold(typeExpand(e, tyenv))
-    case TypeLam(t, e)  => TypeLam(t, typeExpand(e, tyenv))
-    case TypeApp(e, t)  => TypeApp(typeExpand(e, tyenv), t.swap(tyenv))
-  }
-
-  type Env = Map[String, Type]
-
-  def baseEnv : (Env, Env) = (Map(), Map())
-
   def typecheck(p : Prog) : Map[String, Type] = {
     val (finalEnv, finalTyenv) = p.defs.foldLeft(baseEnv)({ case ((env, tyenv), defn) => typecheck(defn)(env, tyenv) })
-    finalEnv + ("main" -> typecheck(typeExpand(p.e, finalTyenv))(finalEnv))
+    finalEnv + ("main" -> typecheck(p.e)(finalEnv)(finalTyenv))
   }
 
 }
