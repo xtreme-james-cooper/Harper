a9b9b519554cb32ff5c46c086844f67b202f747f
James Cooper <jamescooper@gmail.com>
2012-11-20 14:42:56 -0500

    James C - unknowns keep track of pending replacements

--- src/model/Type.scala
+++ src/model/Type.scala
@@ -5,40 +5,41 @@ sealed abstract class Type(name : => String) {
 
   //replace all type variables mu with x
   def swap(mu : String, x : Type) : Type = this match {
-    case Nat                         => Nat
-    case Arrow(t1, t2)               => Arrow(t1.swap(mu, x), t2.swap(mu, x))
-    case UnitTy                      => UnitTy
-    case Product(t1, t2)             => Product(t1.swap(mu, x), t2.swap(mu, x))
-    case Sum(t1, t2)                 => Sum(t1.swap(mu, x), t2.swap(mu, x))
-    case TyVar(y) if y == mu         => x
-    case TyVar(y)                    => TyVar(y)
+    case Nat => Nat
+    case Arrow(t1, t2) => Arrow(t1.swap(mu, x), t2.swap(mu, x))
+    case UnitTy => UnitTy
+    case Product(t1, t2) => Product(t1.swap(mu, x), t2.swap(mu, x))
+    case Sum(t1, t2) => Sum(t1.swap(mu, x), t2.swap(mu, x))
+    case TyVar(y) if y == mu => x
+    case TyVar(y) => TyVar(y)
     case Inductive(y, t1) if y == mu => Inductive(y, t1)
-    case Inductive(y, t1)            => Inductive(y, t1.swap(mu, x))
-    case ForAll(y, t1) if y == mu    => ForAll(y, t1)
-    case ForAll(y, t1)               => ForAll(y, t1.swap(mu, x))
-    case Unknown(id)                 => Unknown(id)
+    case Inductive(y, t1) => Inductive(y, t1.swap(mu, x))
+    case ForAll(y, t1) if y == mu => ForAll(y, t1)
+    case ForAll(y, t1) => ForAll(y, t1.swap(mu, x))
+    case Unknown(id, pendingSwaps) if (!pendingSwaps.contains(mu)) => Unknown(id, pendingSwaps + (mu -> x))
+    case Unknown(id, pendingSwaps) => Unknown(id, pendingSwaps) //ignore it; it won't cause a change
   }
 
   //replace unknown #i with x
   def swap(i : Int, x : Type) : Type = this match {
-    case Nat                    => Nat
-    case Arrow(t1, t2)          => Arrow(t1.swap(i, x), t2.swap(i, x))
-    case UnitTy                 => UnitTy
-    case Product(t1, t2)        => Product(t1.swap(i, x), t2.swap(i, x))
-    case Sum(t1, t2)            => Sum(t1.swap(i, x), t2.swap(i, x))
-    case TyVar(y)               => TyVar(y)
-    case Inductive(y, t1)       => Inductive(y, t1.swap(i, x))
-    case ForAll(y, t1)          => ForAll(y, t1.swap(i, x))
-    case Unknown(id) if i == id => x
-    case Unknown(id)            => Unknown(id)
+    case Nat                                  => Nat
+    case Arrow(t1, t2)                        => Arrow(t1.swap(i, x), t2.swap(i, x))
+    case UnitTy                               => UnitTy
+    case Product(t1, t2)                      => Product(t1.swap(i, x), t2.swap(i, x))
+    case Sum(t1, t2)                          => Sum(t1.swap(i, x), t2.swap(i, x))
+    case TyVar(y)                             => TyVar(y)
+    case Inductive(y, t1)                     => Inductive(y, t1.swap(i, x))
+    case ForAll(y, t1)                        => ForAll(y, t1.swap(i, x))
+    case Unknown(id, pendingSwaps) if i == id => x.swap(pendingSwaps)
+    case Unknown(id, pendingSwaps)            => Unknown(id, pendingSwaps)
   }
 
   def swap(tyenv : Map[String, Type]) : Type = tyenv.foldLeft(this)({ case (t, (syn, x)) => t.swap(syn, x) })
 
   //Compares and builds contraints; also does alpha-renaming
   def ~=~(t2 : Type) : List[(Type, Type)] = (this, t2) match {
-    case (Unknown(i), b)                        => (Unknown(i), b) :: Nil
-    case (a, Unknown(i))                        => (Unknown(i), a) :: Nil
+    case (Unknown(i, pendingSwaps), b)          => (Unknown(i, pendingSwaps), b.swap(pendingSwaps)) :: Nil
+    case (a, Unknown(i, pendingSwaps))          => (Unknown(i, pendingSwaps), a.swap(pendingSwaps)) :: Nil
     case (Inductive(x, st1), Inductive(y, st2)) => st1 ~=~ st2.swap(y, TyVar(x))
     case (ForAll(x, st1), ForAll(y, st2))       => st1 ~=~ st2.swap(y, TyVar(x))
     case (Nat, Nat)                             => Nil
@@ -62,4 +63,4 @@ case class Inductive(x : String, t : Type) extends Type("mu " + x + "." + t)
 case class ForAll(x : String, t : Type) extends Type("forall " + x + "." + t)
 
 //Cannot be written and should not appear in the final result; just for type inference
-case class Unknown(id : Int) extends Type("unknown " + id + " being used")
+case class Unknown(id : Int, pendingSwaps : Map[String, Type]) extends Type("unknown " + id + " being used")
