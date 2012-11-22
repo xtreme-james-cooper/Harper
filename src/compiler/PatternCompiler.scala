49fd5034dedb9afd466a3846dd516a5d3d7e7b64
James Cooper <jamescooper@gmail.com>
2012-11-22 02:41:45 -0500

    James C - even more clean up

--- src/compiler/PatternCompiler.scala
+++ src/compiler/PatternCompiler.scala
@@ -2,45 +2,68 @@ package compiler
 
 import model.{ ZVal, ZPat, WildPat, VarPat, Value, TrivVal, TrivPat, SVal, SPat, Rule, Pattern, PairVal, PairPat, InRVal, InRPat, InLVal, InLPat, Expr }
 
-object PatternCompiler { //extends Compiler[PatStack, (Pattern, Value), Map[String, Value]] {
+object PatternCompiler {
 
-  type Env = List[Map[String, Value]]
-
-  sealed abstract class Target
-  case class Eval(e : (Pattern, Value)) extends Target
-  case class Return(v : Map[String, Value]) extends Target
-
-  def run(v : Value, rs : List[Rule]) : (Expr, Map[String, Value]) = {
-    runRules(v, rs)
+  var v : Value = null
+  
+  def run(v1 : Value, rs : List[Rule]) : (Expr, Map[String, Value]) = {
+    v = v1
+    runRules(rs)
   }
 
-  def runRules(v : Value, rs : List[Rule]) : (Expr, Map[String, Value]) = rs match {
+  def runRules(rs : List[Rule]) : (Expr, Map[String, Value]) = rs match {
     case Nil => throw new Exception("No pattern match found for " + v)
     case Rule(p, b) :: rs => {
-      val (isMatched, bind) = runMatch(false, (p, v), null, Nil)
+      val (isMatched, bind) = runMatch(p, null)
       if (isMatched)
         (b, bind)
       else
-        runRules(v, rs)
+        runRules(rs)
     }
   }
 
-  def runMatch(isMatched : Boolean, e : (Pattern, Value), retval : Map[String, Value], stack : List[PatStack]) : (Boolean, Map[String, Value]) =
-    if (isMatched) stack match {
-      case Nil                        => (true, retval)
-      case PatStackLPair(v2, p2) :: s => runMatch(false, (p2, v2), null, PatStackRPair(retval) :: s)
-      case PatStackRPair(m1) :: s     => runMatch(true, null, m1 ++ retval, s)
+  def runMatch(p : Pattern, retval : Map[String, Value]) : (Boolean, Map[String, Value]) = p match {
+    case WildPat   => (true, Map())
+    case TrivPat   => (true, Map())
+    case VarPat(x) => (true, Map(x -> v))
+    case ZPat      => if (v == ZVal) (true, Map()) else (false, null)
+    case SPat(p) => v match {
+      case SVal(v1) => {
+        v = v1
+        runMatch(p, null)
+      }
+      case _       => (false, null)
     }
-    else e match {
-      case (WildPat, _)                       => runMatch(true, null, Map(), stack)
-      case (VarPat(x), v)                     => runMatch(true, null, Map(x -> v), stack)
-      case (ZPat, ZVal)                       => runMatch(true, null, Map(), stack)
-      case (SPat(p), SVal(v))                 => runMatch(false, (p, v), null, stack)
-      case (TrivPat, TrivVal)                 => runMatch(true, null, Map(), stack)
-      case (InLPat(p), InLVal(v))             => runMatch(false, (p, v), null, stack)
-      case (InRPat(p), InRVal(v))             => runMatch(false, (p, v), null, stack)
-      case (PairPat(p1, p2), PairVal(v1, v2)) => runMatch(false, (p1, v1), null, PatStackLPair(v2, p2) :: stack)
-      case _                                  => (false, null)
+    case InLPat(p) => v match {
+      case InLVal(v1) => {
+        v = v1
+        runMatch(p, null)
+      }
+      case _         => (false, null)
     }
+    case InRPat(p) => v match {
+      case InRVal(v1) => {
+        v = v1
+        runMatch(p, null)
+      }
+      case _         => (false, null)
+    }
+    case PairPat(p1, p2) => v match {
+      case PairVal(v1, v2) => {
+        v = v1
+        val (iMat, retv) = runMatch(p1, null)
+        if (iMat) {
+          v = v2
+          val (imat2, retv2) = runMatch(p2, null)
+          if (imat2)
+            (true, retv ++ retv2)
+          else
+            (false, null)
+        } else
+          (false, null)
+      }
+      case _ => throw new Exception("type error!")
+    }
+  }
 
 }
