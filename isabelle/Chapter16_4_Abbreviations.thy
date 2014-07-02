695528083c1c29fc33c2f5c7e27b0edfa06c9085
James Cooper <jamescooper@gmail.com>
2014-07-02 01:09:29 -0400

    James C - started list, didm’t finish

--- isabelle/Chapter16_4_Abbreviations.thy
+++ isabelle/Chapter16_4_Abbreviations.thy
@@ -45,7 +45,7 @@ proof -
   assume "typecheck env et Nat" 
      and "typecheck env e0 t" 
      and "typecheck (extend_at env 0 Nat) e1 t"
-  moreover hence "typecheck env (Unfold et) (safe_type_subst (Sum Unit (Tyvar 0)) (Rec (Sum Unit (Tyvar 0))))" by simp
+  moreover hence "typecheck env (Unfold et) (safe_type_subst (Sum Unit (Tyvar 0)) Nat)" by simp
   ultimately show "typecheck env (Case (Unfold et) (incr_from 0 e0) e1) t" by (simp add: safe_type_subst_def)
 qed
 lemma "is_val (IsZ e0 e1 e2) = False" by simp
@@ -54,14 +54,14 @@ lemma "EX e'. eval (IsZ Zero e0 e1) e' & eval e' e0"
 proof -
   have "eval (Case (InL Unit Nat Triv) (incr_from 0 e0) e1) (safe_subst (incr_from 0 e0) Triv)" by (metis ecs2 is_val.simps(5))
   hence "eval (Case (InL Unit Nat Triv) (incr_from 0 e0) e1) e0" by simp
-  moreover have "eval (Case (Unfold (Fold (Sum Unit (Tyvar 0)) (InL Unit Nat Triv))) (incr_from 0 e0) e1) (Case (InL Unit Nat Triv) (incr_from 0 e0) e1)" by simp
-  ultimately have "EX e'. eval (Case (Unfold (Fold (Sum Unit (Tyvar 0)) (InL Unit Nat Triv))) (incr_from 0 e0) e1) e' & eval e' e0" by fast
+  moreover have "eval (Case (Unfold Zero) (incr_from 0 e0) e1) (Case (InL Unit Nat Triv) (incr_from 0 e0) e1)" by simp
+  ultimately have "EX e'. eval (Case (Unfold Zero) (incr_from 0 e0) e1) e' & eval e' e0" by fast
   thus "EX e'. eval (IsZ Zero e0 e1) e' & eval e' e0" by simp
 qed
 lemma "is_val e ==> EX e'. eval (IsZ (Succ e) e0 e1) e' & eval e' (safe_subst e1 e)" 
 proof -
   assume X: "is_val e"
-  hence "eval (Case (Unfold (Fold (Sum Unit (Tyvar 0)) (InR Unit Nat e))) (incr_from 0 e0) e1) (Case (InR Unit Nat e) (incr_from 0 e0) e1)" by simp
+  hence "eval (Case (Unfold (Succ e)) (incr_from 0 e0) e1) (Case (InR Unit Nat e) (incr_from 0 e0) e1)" by simp
   moreover with X have "eval (Case (InR Unit Nat e) (incr_from 0 e0) e1) (safe_subst e1 e)" by simp
   ultimately show "EX e'. eval (IsZ (Succ e) e0 e1) e' & eval e' (safe_subst e1 e)" by auto
 qed
@@ -73,5 +73,67 @@ by (induction env e t rule: typecheck.induct, auto simp add: canonical_unit safe
 
 lemma canonical_nat: "typecheck env e Nat ==> is_val e ==> e = Zero | (EX v. typecheck env v Nat & is_val v & e = Succ v)"
 by (simp add: canonical_nat_like)
+
+(* -------- *)
+
+abbreviation List :: "type => type"
+where "List a == Rec (Sum Unit (Prod (type_incr_from 0 a) (Tyvar 0)))"
+
+lemma "is_valid_type {} (List a) = is_valid_type {} a"
+proof -
+  have "is_valid_type (expand_set {}) (type_incr_from 0 a) = is_valid_type {} a" by simp
+  thus "is_valid_type {} (List a) = is_valid_type {} a" by (simp add: expand_set_def)
+qed
+lemma "free_type_vars (List a) = free_type_vars a" by simp
+lemma "type_sub_from n (List a) = List (type_sub_from n a)"
+proof -
+  have "0 < n" by simp sorry
+  thus "type_sub_from n (List a) = List (type_sub_from n a)" by simp
+qed
+lemma "type_incr_from n (List a) = List (type_incr_from n a)" by simp
+lemma "type_subst (List a) t x = List (type_subst a t x)" by simp
+
+abbreviation Nil :: "type => expr"
+where "Nil a == Fold (Sum Unit (Prod (type_incr_from 0 a) (Tyvar 0))) (InL Unit (List a) Triv)"
+
+lemma "free_vars (Nil t) = {}" by simp
+lemma "incr_from n (Nil t) = (Nil t)" by simp
+lemma "sub_from n (Nil t) = (Nil t)" by simp
+lemma "subst (Nil t) e x = (Nil t)" by simp
+lemma "typecheck env (Nil t) (List t)"
+proof -
+  have "typecheck env (Fold (Sum Unit (Prod (type_incr_from 0 t) (Tyvar 0))) (InL Unit (List t) Triv)) (List t)" by simp sorry
+  thus "typecheck env (Nil t) (List t)" by simp
+qed
+lemma "is_val (Nil t)" by simp
+
+abbreviation Cons :: "type => expr => expr => expr"
+where "Cons t x xs == Fold (Sum Unit (Prod (type_incr_from 0 t) (Tyvar 0))) (InR Unit (Prod (type_incr_from 0 t) (List t)) (Pair x xs))"
+
+lemma "free_vars (Cons t x xs) = free_vars x Un free_vars xs" by simp
+lemma "incr_from n (Cons t x xs) = Cons t (incr_from n x) (incr_from n xs)" by simp
+lemma "sub_from n (Cons t x xs) = Cons t (sub_from n x) (sub_from n xs)" by simp
+lemma "subst (Cons t x xs) e y = Cons t (subst x e y) (subst xs e y)" by simp
+lemma "typecheck env x t ==> typecheck env xs (List t) ==> typecheck env (Cons t x xs) (List t)" by simp sorry
+lemma "is_val (Cons t x xs) = (is_val x & is_val xs)" by simp
+lemma "eval x x' ==> eval (Cons t x xs) (Cons t x' xs)" by simp
+lemma "is_val x ==> eval xs xs' ==> eval (Cons t x xs) (Cons t x xs')" by simp
+
+abbreviation ListCase :: "expr => expr => expr => expr"
+where "ListCase et e0 e1 == Case (Unfold et) (incr_from 0 e0) e1"
+
+lemma "free_vars (ListCase et e0 e1) = free_vars e0 Un free_vars et Un redr_set (free_vars e1)" by auto
+lemma "incr_from n (ListCase et e0 e1) = ListCase (incr_from n et) (incr_from n e0) (incr_from (Suc n) e1)" by simp
+lemma "n ~: free_vars (ListCase et e0 e1) ==> sub_from n (ListCase et e0 e1) = ListCase (sub_from n et) (sub_from n e0) (sub_from (Suc n) e1)" by simp
+lemma "subst (ListCase et e0 e1) e x = ListCase (subst et e x) (subst e0 e x) (subst e1 (incr_from 0 e) (Suc x))" by simp
+lemma "typecheck env et (List t) ==> typecheck env e0 t2 ==> typecheck (extend_at env 0 (Prod t (List t))) e1 t2 ==> typecheck env (ListCase et e0 e1) t2"  by simp sorry
+lemma "is_val (ListCase e0 e1 e2) = False" by simp
+lemma "eval e e' ==> eval (ListCase e e0 e1) (ListCase e' e0 e1)" by simp
+lemma "EX e'. eval (ListCase (Nil t) e0 e1) e' & eval e' e0" by simp sorry
+lemma "is_val e ==> EX e'. eval (ListCase (Cons t x xs) e0 e1) e' & eval e' (safe_subst e1 e)" by simp sorry
+
+lemma canonical_list: "typecheck env e (List t) ==> is_val e ==> 
+          e = Nil t | (EX x xs. typecheck env x t & typecheck env xs (List t) & is_val x & is_val xs & e = Cons t x xs)"
+by simp sorry
 
 end
