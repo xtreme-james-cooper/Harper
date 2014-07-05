fd0ec323dda228bd9d0366ca719366cb91308f6a
James Cooper <jamescooper@gmail.com>
2014-07-05 10:26:50 -0400

    James C - abstracted out vars

--- isabelle/Chapter04.thy
+++ isabelle/Chapter04.thy
@@ -5,7 +5,7 @@ begin
 datatype type = NumType | StrType
 
 datatype expr = 
-  Var nat
+  Var var
 | Num int
 | Str string
 | Plus expr expr
@@ -37,27 +37,27 @@ inductive_cases [elim!]: "typecheck gam (Cat e1 e2) t"
 inductive_cases [elim!]: "typecheck gam (Len e) t"
 inductive_cases [elim!]: "typecheck gam (Let e1 e2) t"
 
-primrec incr_from :: "nat => expr => expr"
-where "incr_from n (Var v) = Var (if v < n then v else Suc v)"
-    | "incr_from n (Num x) = Num x"
-    | "incr_from n (Str s) = Str s"
-    | "incr_from n (Plus e1 e2) = Plus (incr_from n e1) (incr_from n e2)"
-    | "incr_from n (Times e1 e2) = Times (incr_from n e1) (incr_from n e2)"
-    | "incr_from n (Cat e1 e2) = Cat (incr_from n e1) (incr_from n e2)"
-    | "incr_from n (Len e) = Len (incr_from n e)"
-    | "incr_from n (Let e1 e2) = Let (incr_from n e1) (incr_from (Suc n) e2)"
+primrec insert :: "var => expr => expr"
+where "insert n (Var v) = Var (incr n v)"
+    | "insert n (Num x) = Num x"
+    | "insert n (Str s) = Str s"
+    | "insert n (Plus e1 e2) = Plus (insert n e1) (insert n e2)"
+    | "insert n (Times e1 e2) = Times (insert n e1) (insert n e2)"
+    | "insert n (Cat e1 e2) = Cat (insert n e1) (insert n e2)"
+    | "insert n (Len e) = Len (insert n e)"
+    | "insert n (Let e1 e2) = Let (insert n e1) (insert (next n) e2)"
 
-primrec sub_from :: "nat => expr => expr"
-where "sub_from n (Var v) = Var (if v < n then v else v - 1)"
-    | "sub_from n (Num x) = Num x"
-    | "sub_from n (Str s) = Str s"
-    | "sub_from n (Plus e1 e2) = Plus (sub_from n e1) (sub_from n e2)"
-    | "sub_from n (Times e1 e2) = Times (sub_from n e1) (sub_from n e2)"
-    | "sub_from n (Cat e1 e2) = Cat (sub_from n e1) (sub_from n e2)"
-    | "sub_from n (Len e) = Len (sub_from n e)"
-    | "sub_from n (Let e1 e2) = Let (sub_from n e1) (sub_from (Suc n) e2)"
+primrec remove :: "var => expr => expr"
+where "remove n (Var v) = Var (subr n v)"
+    | "remove n (Num x) = Num x"
+    | "remove n (Str s) = Str s"
+    | "remove n (Plus e1 e2) = Plus (remove n e1) (remove n e2)"
+    | "remove n (Times e1 e2) = Times (remove n e1) (remove n e2)"
+    | "remove n (Cat e1 e2) = Cat (remove n e1) (remove n e2)"
+    | "remove n (Len e) = Len (remove n e)"
+    | "remove n (Let e1 e2) = Let (remove n e1) (remove (next n) e2)"
 
-primrec subst' :: "nat => expr => expr => expr"
+primrec subst' :: "var => expr => expr => expr"
 where "subst' n e' (Var v) = (if v = n then e' else Var v)"
     | "subst' n e' (Num x) = Num x"
     | "subst' n e' (Str s) = Str s"
@@ -65,73 +65,24 @@ where "subst' n e' (Var v) = (if v = n then e' else Var v)"
     | "subst' n e' (Times e1 e2) = Times (subst' n e' e1) (subst' n e' e2)"
     | "subst' n e' (Cat e1 e2) = Cat (subst' n e' e1) (subst' n e' e2)"
     | "subst' n e' (Len e) = Len (subst' n e' e)"
-    | "subst' n e' (Let e1 e2) = Let (subst' n e' e1) (subst' (Suc n) (incr_from 0 e') e2)"
+    | "subst' n e' (Let e1 e2) = Let (subst' n e' e1) (subst' (next n) (insert first e') e2)"
 
 definition subst :: "expr => expr => expr"
-where "subst e' e = sub_from 0 (subst' 0 (incr_from 0 e') e)"
+where "subst e' e = remove first (subst' first (insert first e') e)"
 
-primrec free_vars :: "expr => nat set"
-where "free_vars (Var v) = {v}"
-    | "free_vars (Num x) = {}"
-    | "free_vars (Str s) = {}"
-    | "free_vars (Plus e1 e2) = free_vars e1 Un free_vars e2"
-    | "free_vars (Times e1 e2) = free_vars e1 Un free_vars e2"
-    | "free_vars (Cat e1 e2) = free_vars e1 Un free_vars e2"
-    | "free_vars (Len e) = free_vars e"
-    | "free_vars (Let e1 e2) = free_vars e1 Un (%x. x - 1) ` (free_vars e2 - {0})"
+lemma [simp]: "typecheck gam e t ==> n in gam ==> 
+         typecheck (extend_at n gam t') (insert n e) t"
+by (induction gam e t arbitrary: n rule: typecheck.induct, fastforce+)
 
-lemma [simp]: "typecheck gam e t ==> n <= size gam ==> 
-         typecheck (extend_at n gam t') (incr_from n e) t"
-proof (induction gam e t arbitrary: n rule: typecheck.induct)
-case (tc_var x t)
-  thus ?case by simp
-next case tc_str
-  thus ?case by simp
-next case tc_num
-  thus ?case by simp
-next case tc_plus
-  thus ?case by simp
-next case tc_times
-  thus ?case by simp
-next case tc_cat
-  thus ?case by simp
-next case tc_len
-  thus ?case by simp
-next case (tc_let gam e1 t1 e2 t2)
-  from tc_let have X: "typecheck (extend_at n gam t') (incr_from n e1) t1" by simp
-  from tc_let have "typecheck (extend (extend_at n gam t') t1) (incr_from (Suc n) e2) t2" by simp
-  with X show ?case by simp
-qed
-
-lemma [simp]: "sub_from n (incr_from n e) = e"
+lemma [simp]: "remove n (insert n e) = e"
 by (induction e arbitrary: n, simp_all)
 
-lemma [simp]: "m <= n ==> incr_from m (incr_from n e) = incr_from (Suc n) (incr_from m e)"
+lemma [simp]: "canswap m n ==> insert m (insert n e) = insert (next n) (insert m e)"
 by (induction e arbitrary: n m, simp_all)
 
-lemma [simp]: "typecheck (extend_at n gam t') e t ==> n <= size gam ==> typecheck gam e' t' ==> 
-        typecheck gam (sub_from n (subst' n (incr_from n e') e)) t"
-proof (induction "extend_at n gam t'" e t arbitrary: n gam t' e' rule: typecheck.induct)
-case (tc_var x t)
-  thus ?case by auto
-next case tc_str
-  thus ?case by simp
-next case tc_num
-  thus ?case by simp
-next case tc_plus
-  thus ?case by simp
-next case tc_times
-  thus ?case by simp
-next case tc_cat
-  thus ?case by simp
-next case tc_len
-  thus ?case by simp
-next case (tc_let e1 t1 e2 t2 n)
-  with tc_let have X: "typecheck (extend gam t1) (sub_from (Suc n) (subst' (Suc n) 
-                             (incr_from (Suc n) (incr_from 0 e')) e2)) t2" by simp
-  from tc_let have "typecheck gam (sub_from n (subst' n (incr_from n e') e1)) t1" by simp
-  with X show ?case by simp
-qed
+lemma [simp]: "typecheck (extend_at n gam t') e t ==> n in gam ==> typecheck gam e' t' ==> 
+        typecheck gam (remove n (subst' n (insert n e') e)) t"
+by (induction "extend_at n gam t'" e t arbitrary: n gam t' e' rule: typecheck.induct, fastforce+)
 
 lemma [simp]: "typecheck (extend gam t') e t ==> typecheck gam e' t' ==> 
                           typecheck gam (subst e' e) t"
--- isabelle/DeBruijnEnvironment.thy
+++ isabelle/DeBruijnEnvironment.thy
@@ -2,41 +2,64 @@ theory DeBruijnEnvironment
 imports Main
 begin
 
+datatype var = DBVar nat
+
+definition first :: var
+where "first = DBVar 0"
+
+primrec "next" :: "var => var"
+where "next (DBVar v) = DBVar (Suc v)"
+
+fun incr :: "var => var => var"
+where "incr (DBVar n) (DBVar v) = DBVar (if v < n then v else Suc v)" 
+
+fun subr :: "var => var => var"
+where "subr (DBVar n) (DBVar v) = DBVar (if v < n then v else v - 1)" 
+
 datatype 'a env = DBEnv "'a list"
 
 definition empty_env :: "'a env"
 where "empty_env = DBEnv []"
 
-primrec size :: "'a env => nat"
-where "size (DBEnv as) = length as"
+fun is_in :: "var => 'a env => bool" (infix "in" 65)
+where "is_in (DBVar n) (DBEnv as) = (n <= length as)"
 
 fun lookup' :: "'a list => nat => 'a option"
 where "lookup' [] n = None"
     | "lookup' (a # as) 0 = Some a"
     | "lookup' (a # as) (Suc n) = lookup' as n"
 
-primrec lookup :: "'a env => nat => 'a option"
-where "lookup (DBEnv as) n = lookup' as n"
+fun lookup :: "'a env => var => 'a option"
+where "lookup (DBEnv as) (DBVar n) = lookup' as n"
 
 fun extend_at' :: "nat => 'a list => 'a => 'a list"
 where "extend_at' 0 as a' = a' # as"
     | "extend_at' (Suc n) [] a' = undefined"
     | "extend_at' (Suc n) (a # as) a' = a # extend_at' n as a'"
 
-primrec extend_at :: "nat => 'a env => 'a => 'a env"
-where "extend_at n (DBEnv as) a' = DBEnv (extend_at' n as a')"
+fun extend_at :: "var => 'a env => 'a => 'a env"
+where "extend_at (DBVar n) (DBEnv as) a' = DBEnv (extend_at' n as a')"
 
 abbreviation extend :: "'a env => 'a => 'a env"
-where "extend == extend_at 0"
+where "extend == extend_at first"
+
+lemma [simp]: "first in gam"
+by (induction gam, simp add: first_def)
+
+lemma [simp]: "x <= length list ==> length (extend_at' x list y) = Suc (length list)"
+by (induction x list y rule: extend_at'.induct, simp_all)
+
+lemma [simp]: "x in gam ==> n in gam ==> next n in extend_at x gam y"
+by (induction gam, induction x, induction n, simp)
+
+lemma [simp]: "subr n (incr n var) = var"
+by (induction n, induction var, simp)
 
 lemma [simp]: "lookup empty_env x = None"
-by (simp add: empty_env_def)
+by (cases x, simp add: empty_env_def)
 
 lemma [simp]: "n <= length l ==> length (extend_at' n l a) = Suc (length l)"
 by (induction n l a rule: extend_at'.induct, simp_all)
-
-lemma [simp]: "n <= size env ==> size (extend_at n env a) = Suc (size env)"
-by (induction env, simp)
 
 lemma [simp]: "n <= length l ==> lookup' (extend_at' n l t) x = 
                   (if x < n then lookup' l x else if x > n then lookup' l (x - 1) else Some t)"
@@ -55,22 +78,16 @@ next case 3
   qed
 qed
 
-lemma [simp]: "n <= size gam ==> x ~= n ==> 
-          lookup (extend_at n gam t) x = lookup gam (if x < n then x else x - 1)"
-by (induction gam, simp)
+lemma [simp]: "n in gam ==> x ~= n ==> 
+          lookup (extend_at n gam t) x = lookup gam (subr n x)"
+by (induction gam, induction n, induction x, simp)
 
-lemma [simp]: "n <= size gam ==> lookup (extend_at n gam t) n = Some t"
-by (induction gam, simp)
+lemma [simp]: "n in gam ==> lookup (extend_at n gam t) n = Some t"
+by (induction gam, induction n, simp)
 
-lemma [simp]: "ALL x : (%x. x - Suc 0) ` (s - {0}). lookup gam x = lookup gam' x ==> 
-                    ALL x : s. lookup (extend_at 0 gam t1) x = lookup (extend_at 0 gam' t1) x"
-proof (induction gam, induction gam', auto)
-  fix list::"'a list" 
-  fix lista x
-  assume "ALL x : s - {0}. lookup' lista (x - Suc 0) = lookup' list (x - Suc 0)"
-     and "x : s"
-  thus "lookup' (t1 # lista) x = lookup' (t1 # list) x" by (induction x, simp, force)
-qed
+lemma [simp]: "n in gam ==> lookup gam x = Some t ==> 
+         lookup (extend_at n gam t') (incr n x) = Some t"
+by (induction gam, induction n, induction x, simp)
 
 lemma [simp]: "n <= length l ==> m <= n ==> 
           extend_at' m (extend_at' n l a) b = extend_at' (Suc n) (extend_at' m l b) a"
@@ -83,8 +100,23 @@ next case (3 n a' as a)
   thus ?case by (cases m, simp_all)
 qed
 
-lemma [simp]: "n <= size gam ==> m <= n ==> 
-        extend_at m (extend_at n gam a) b = extend_at (Suc n) (extend_at m gam b) a"
-by (induction gam, simp)
+fun canswap :: "var => var => bool"
+where "canswap (DBVar m) (DBVar n) = (m <= n)" 
+
+lemma [simp]: "n in gam ==> canswap m n ==>
+        extend_at m (extend_at n gam a) b = extend_at (next n) (extend_at m gam b) a"
+by (induction gam, induction n, induction m, simp)
+
+lemma [simp]: "canswap m n ==> incr m (incr n var) = incr (next n) (incr m var)"
+by (induction var, induction n, induction m, simp)
+
+lemma [simp]: "canswap first x"
+by (induction x, simp add: first_def)
+
+lemma [simp]: "canswap m n ==> canswap (next m) (next n)"
+by (induction n, induction m, simp)
+
+lemma [simp]: "n in gam ==> canswap m n ==> m in gam"
+by (induction gam, induction n, induction m, simp)
 
 end
