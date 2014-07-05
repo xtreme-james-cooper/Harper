c42e9a01f4b5a5c12199feff39ca4d2a5d212e01
James Cooper <jamescooper@gmail.com>
2014-07-05 13:42:46 -0400

    James C - Started chapter 10

---
+++ isabelle/Chapter10_1_Language.thy
@@ -0,0 +1,53 @@
+theory Chapter10_1_Language
+imports DeBruijnEnvironment
+begin
+
+datatype type = 
+  Nat
+| Arrow type type
+
+
+datatype expr = 
+  Var var
+| Zero
+| Suc expr
+| Rec expr expr expr
+| Lam type expr
+| Appl expr expr
+
+primrec insert :: "var => expr => expr"
+where "insert n (Var v) = Var (incr n v)"
+    | "insert n Zero = Zero"
+    | "insert n (Suc e) = Suc (insert n e)"
+    | "insert n (Rec et e0 es) = Rec (insert n et) (insert n e0) (insert (next (next n)) es)"
+    | "insert n (Lam t e) = Lam t (insert (next n) e)"
+    | "insert n (Appl e1 e2) = Appl (insert n e1) (insert n e2)"
+
+primrec remove :: "var => expr => expr"
+where "remove n (Var v) = Var (subr n v)"
+    | "remove n Zero = Zero"
+    | "remove n (Suc e) = Suc (remove n e)"
+    | "remove n (Rec et e0 es) = Rec (remove n et) (remove n e0) (remove (next (next n)) es)"
+    | "remove n (Lam t e) = Lam t (remove (next n) e)"
+    | "remove n (Appl e1 e2) = Appl (remove n e1) (remove n e2)"
+
+primrec subst' :: "var => expr => expr => expr"
+where "subst' n e' (Var v) = (if v = n then e' else Var v)"
+    | "subst' n e' Zero = Zero"
+    | "subst' n e' (Suc e) = Suc (subst' n e' e)"
+    | "subst' n e' (Rec et e0 es) = 
+                      Rec (subst' n e' et) (subst' n e' e0) 
+                          (subst' (next (next n)) (insert first (insert first e')) es)"
+    | "subst' n e' (Lam t e) = Lam t (subst' (next n) (insert first e') e)"
+    | "subst' n e' (Appl e1 e2) = Appl (subst' n e' e1) (subst' n e' e2)"
+
+definition subst :: "expr => expr => expr"
+where "subst e' e = remove first (subst' first (insert first e') e)"
+
+lemma [simp]: "remove n (insert n e) = e"
+by (induction e arbitrary: n, simp_all)
+
+lemma [simp]: "canswap m n ==> insert m (insert n e) = insert (next n) (insert m e)"
+by (induction e arbitrary: n m, simp_all)
+
+end
---
+++ isabelle/Chapter10_2_Typechecking.thy
@@ -0,0 +1,34 @@
+theory Chapter10_2_Typechecking
+imports Chapter10_1_Language
+begin
+
+inductive typecheck :: "type env => expr => type => bool"
+where tc_var [simp]: "lookup gam x = Some t ==> typecheck gam (Var x) t"
+    | tc_zero [simp]: "typecheck gam Zero Nat"
+    | tc_suc [simp]: "typecheck gam e Nat ==> typecheck gam (Suc e) Nat"
+    | tc_rec [simp]: "typecheck gam et Nat ==> typecheck gam e0 t ==> 
+                typecheck (extend (extend gam t) Nat) es t ==> typecheck gam (Rec et e0 es) t"
+    | tc_lam [simp]: "typecheck (extend gam t1) e t2 ==> typecheck gam (Lam t1 e) (Arrow t1 t2)"
+    | tc_appl [simp]: "typecheck gam e1 (Arrow t2 t) ==> typecheck gam e2 t2 ==> 
+                typecheck gam (Appl e1 e2) t"
+
+inductive_cases [elim!]: "typecheck gam (Var x) t"
+inductive_cases [elim!]: "typecheck gam Zero t"
+inductive_cases [elim!]: "typecheck gam (Suc e) t"
+inductive_cases [elim!]: "typecheck gam (Rec et e0 es) t"
+inductive_cases [elim!]: "typecheck gam (Lam t1 e) t"
+inductive_cases [elim!]: "typecheck gam (Appl e1 e2) t"
+
+lemma [simp]: "typecheck gam e t ==> n in gam ==> 
+         typecheck (extend_at n gam t') (insert n e) t"
+by (induction gam e t arbitrary: n rule: typecheck.induct, fastforce+)
+
+lemma [simp]: "typecheck (extend_at n gam t') e t ==> n in gam ==> typecheck gam e' t' ==> 
+        typecheck gam (remove n (subst' n (insert n e') e)) t"
+by (induction "extend_at n gam t'" e t arbitrary: n gam t' e' rule: typecheck.induct, fastforce+)
+
+lemma [simp]: "typecheck (extend gam t') e t ==> typecheck gam e' t' ==> 
+                          typecheck gam (subst e' e) t"
+by (simp add: subst_def)
+
+end
