bfb4b935f19ad8b5b3c5047424f39165a7eb5bb0
James Cooper <jamescooper@gmail.com>
2015-10-13 15:00:57 -0400

    James C - more reformatting

--- isabelle/Chapter04.thy
+++ isabelle/Chapter04.thy
@@ -14,39 +14,39 @@ datatype expr =
 | Len expr
 | Let expr expr
 
-primrec insert :: "var => expr => expr"
-where "insert n (Var v) = Var (incr n v)"
-    | "insert n (Num x) = Num x"
-    | "insert n (Str s) = Str s"
-    | "insert n (Plus e1 e2) = Plus (insert n e1) (insert n e2)"
-    | "insert n (Times e1 e2) = Times (insert n e1) (insert n e2)"
-    | "insert n (Cat e1 e2) = Cat (insert n e1) (insert n e2)"
-    | "insert n (Len e) = Len (insert n e)"
-    | "insert n (Let e1 e2) = Let (insert n e1) (insert (next n) e2)"
+primrec insert :: "var \<Rightarrow> expr \<Rightarrow> expr" where 
+  "insert n (Var v) = Var (incr n v)"
+| "insert n (Num x) = Num x"
+| "insert n (Str s) = Str s"
+| "insert n (Plus e1 e2) = Plus (insert n e1) (insert n e2)"
+| "insert n (Times e1 e2) = Times (insert n e1) (insert n e2)"
+| "insert n (Cat e1 e2) = Cat (insert n e1) (insert n e2)"
+| "insert n (Len e) = Len (insert n e)"
+| "insert n (Let e1 e2) = Let (insert n e1) (insert (next n) e2)"
 
-primrec subst :: "expr => var => expr => expr"
-where "subst e' n (Var x) = (if x = n then e' else Var (subr n x))"
-    | "subst e' n (Num x) = Num x"
-    | "subst e' n (Str s) = Str s"
-    | "subst e' n (Plus e1 e2) = Plus (subst e' n e1) (subst e' n e2)"
-    | "subst e' n (Times e1 e2) = Times (subst e' n e1) (subst e' n e2)"
-    | "subst e' n (Cat e1 e2) = Cat (subst e' n e1) (subst e' n e2)"
-    | "subst e' n (Len e) = Len (subst e' n e)"
-    | "subst e' n (Let e1 e2) = Let (subst e' n e1) (subst (insert first e') (next n) e2)"
+primrec subst :: "expr \<Rightarrow> var \<Rightarrow> expr \<Rightarrow> expr" where 
+  "subst e' n (Var x) = (if x = n then e' else Var (subr n x))"
+| "subst e' n (Num x) = Num x"
+| "subst e' n (Str s) = Str s"
+| "subst e' n (Plus e1 e2) = Plus (subst e' n e1) (subst e' n e2)"
+| "subst e' n (Times e1 e2) = Times (subst e' n e1) (subst e' n e2)"
+| "subst e' n (Cat e1 e2) = Cat (subst e' n e1) (subst e' n e2)"
+| "subst e' n (Len e) = Len (subst e' n e)"
+| "subst e' n (Let e1 e2) = Let (subst e' n e1) (subst (insert first e') (next n) e2)"
 
-inductive typecheck :: "type env => expr => type => bool"
-where tc_var [simp]: "lookup gam x = Some t ==> typecheck gam (Var x) t"
-    | tc_str [simp]: "typecheck gam (Str s) StrType"
-    | tc_num [simp]: "typecheck gam (Num n) NumType"
-    | tc_plus [simp]: "typecheck gam e1 NumType ==> typecheck gam e2 NumType ==> 
-                    typecheck gam (Plus e1 e2) NumType"
-    | tc_times [simp]: "typecheck gam e1 NumType ==> typecheck gam e2 NumType ==> 
-                    typecheck gam (Times e1 e2) NumType"
-    | tc_cat [simp]: "typecheck gam e1 StrType ==> typecheck gam e2 StrType ==>
-                    typecheck gam (Cat e1 e2) StrType"
-    | tc_len [simp]: "typecheck gam e StrType ==> typecheck gam (Len e) NumType"
-    | tc_let [simp]: "typecheck gam e1 t1 ==> typecheck (extend gam t1) e2 t2 ==> 
-                    typecheck gam (Let e1 e2) t2"
+inductive typecheck :: "type env \<Rightarrow> expr \<Rightarrow> type \<Rightarrow> bool" where 
+  tc_var [simp]: "lookup gam x = Some t \<Longrightarrow> typecheck gam (Var x) t"
+| tc_str [simp]: "typecheck gam (Str s) StrType"
+| tc_num [simp]: "typecheck gam (Num n) NumType"
+| tc_plus [simp]: "typecheck gam e1 NumType \<Longrightarrow> typecheck gam e2 NumType \<Longrightarrow>
+    typecheck gam (Plus e1 e2) NumType"
+| tc_times [simp]: "typecheck gam e1 NumType \<Longrightarrow> typecheck gam e2 NumType \<Longrightarrow>
+    typecheck gam (Times e1 e2) NumType"
+| tc_cat [simp]: "typecheck gam e1 StrType \<Longrightarrow> typecheck gam e2 StrType \<Longrightarrow> 
+    typecheck gam (Cat e1 e2) StrType"
+| tc_len [simp]: "typecheck gam e StrType \<Longrightarrow> typecheck gam (Len e) NumType"
+| tc_let [simp]: "typecheck gam e1 t1 \<Longrightarrow> typecheck (extend gam t1) e2 t2 \<Longrightarrow>
+    typecheck gam (Let e1 e2) t2"
 
 inductive_cases [elim!]: "typecheck gam (Var x) t"
 inductive_cases [elim!]: "typecheck gam (Str s) t"
@@ -57,11 +57,11 @@ inductive_cases [elim!]: "typecheck gam (Cat e1 e2) t"
 inductive_cases [elim!]: "typecheck gam (Len e) t"
 inductive_cases [elim!]: "typecheck gam (Let e1 e2) t"
 
-lemma [simp]: "typecheck gam e t ==> n in gam ==> typecheck (extend_at n gam t') (insert n e) t"
-by (induction gam e t arbitrary: n rule: typecheck.induct, fastforce+)
+lemma [simp]: "typecheck gam e t \<Longrightarrow> n in gam \<Longrightarrow> typecheck (extend_at n gam t') (insert n e) t"
+  by (induction gam e t arbitrary: n rule: typecheck.induct) fastforce+
 
-lemma [simp]: "typecheck (extend_at n gam t') e t ==> n in gam ==> typecheck gam e' t' ==> 
-                          typecheck gam (subst e' n e) t"
-by (induction "extend_at n gam t'" e t arbitrary: gam e' n rule: typecheck.induct, fastforce+)
+lemma [simp]: "typecheck (extend_at n gam t') e t \<Longrightarrow> n in gam \<Longrightarrow> typecheck gam e' t' \<Longrightarrow> 
+    typecheck gam (subst e' n e) t"
+  by (induction "extend_at n gam t'" e t arbitrary: gam e' n rule: typecheck.induct) fastforce+
 
 end
